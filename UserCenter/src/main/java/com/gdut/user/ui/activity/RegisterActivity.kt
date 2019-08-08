5cd49224b36893e0be812b4247313d1f444a24e2
xuyang-happy <958607524@qq.com>
2019-08-08 14:05:25 +0800

    anko库的引入

--- UserCenter/src/main/java/com/gdut/user/ui/activity/RegisterActivity.kt
+++ UserCenter/src/main/java/com/gdut/user/ui/activity/RegisterActivity.kt
@@ -3,11 +3,20 @@ package com.gdut.user.ui.activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.gdut.user.R
+import kotlinx.android.synthetic.main.activity_register.*
+import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
+
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        mRegisterBtn.setOnClickListener {
+            toast("注册")
+        }
     }
 }
