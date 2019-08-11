84f4c77e4d02fd4e3aa5bce50376d7d5e31a503b
xuyang-happy <958607524@qq.com>
2019-08-11 11:45:39 +0800

    使用腾讯云服务器

--- BaseLibrary/src/main/java/com/gdut/base/common/BaseConstant.kt
+++ BaseLibrary/src/main/java/com/gdut/base/common/BaseConstant.kt
@@ -8,6 +8,8 @@ package com.gdut.base.common
 class BaseConstant{
     companion object{
         //本地ip
-        val SERVER_ADDRESS = "http://10.23.15.6:8080"
+        //val SERVER_ADDRESS = "http://10.23.15.6:8080/mall/"
+        //腾讯云ip
+        val SERVER_ADDRESS = "http://203.195.245.169:8080/mall/"
     }
 }
