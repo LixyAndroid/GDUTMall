5532a24100884b9bb83c7576b4e27b9d6c664590
xuyang-happy <958607524@qq.com>
2019-08-12 22:07:45 +0800

    设置头像，拍照，选择相册图片实现

--- UserCenter/src/main/java/com/gdut/user/ui/activity/UserInfoActivity.kt
+++ UserCenter/src/main/java/com/gdut/user/ui/activity/UserInfoActivity.kt
@@ -1,7 +1,15 @@
 package com.gdut.user.ui.activity
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import android.view.View
+import android.os.Environment
+import android.support.annotation.RequiresApi
+import android.support.v4.app.ActivityCompat
+import android.util.Log
 import com.bigkoo.alertview.AlertView
 import com.bigkoo.alertview.OnItemClickListener
 import com.gdut.base.ext.onClick
@@ -11,24 +19,38 @@ import com.gdut.user.injection.component.DaggerUserComponent
 import com.gdut.user.injection.module.UserModule
 import com.gdut.user.presenter.UserInfoPresenter
 import com.gdut.user.presenter.view.UserInfoView
+import com.jph.takephoto.app.TakePhoto
+import com.jph.takephoto.app.TakePhotoImpl
+import com.jph.takephoto.compress.CompressConfig
+import com.jph.takephoto.model.TResult
+import com.kotlin.base.utils.DateUtils
 import kotlinx.android.synthetic.main.activity_user_info.*
-import org.jetbrains.anko.toast
+import java.io.File
 
 /**
  * 用户信息
  */
 
-class UserInfoActivity : BaseMvpActivity<UserInfoPresenter>(), UserInfoView {
+class UserInfoActivity : BaseMvpActivity<UserInfoPresenter>(), UserInfoView, TakePhoto.TakeResultListener {
 
+    private lateinit var mTakePhoto: TakePhoto
 
+    private lateinit var mTempFile: File
 
-
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_info)
 
+        //拍照权限
+        requestDangerousPermissions()
+
+        mTakePhoto = TakePhotoImpl(this, this)
 
         initView()
+
+        mTakePhoto.onCreate(savedInstanceState)
+
 
     }
 
@@ -46,13 +68,19 @@ class UserInfoActivity : BaseMvpActivity<UserInfoPresenter>(), UserInfoView {
 
 
     private fun showAlertView() {
-         AlertView("选择图片","", "取消", null, arrayOf("拍照", "从相册中选择"), this,
+        AlertView("选择图片", "", "取消", null, arrayOf("拍照", "从相册中选择"), this,
             AlertView.Style.ActionSheet, object : OnItemClickListener {
                 override fun onItemClick(o: Any?, position: Int) {
-                        when(position){
-                            0 -> toast("拍照")
-                            1 -> toast("相册")
+                    mTakePhoto.onEnableCompress(CompressConfig.ofDefaultConfig(), false)
+
+                    when (position) {
+                        0 -> {
+                            createTempFile()
+                            mTakePhoto.onPickFromCapture(Uri.fromFile(mTempFile))
                         }
+
+                        1 -> mTakePhoto.onPickFromGallery()
+                    }
                 }
 
             }
@@ -63,6 +91,59 @@ class UserInfoActivity : BaseMvpActivity<UserInfoPresenter>(), UserInfoView {
     override fun injectComponent() {
         DaggerUserComponent.builder().activityComponent(activityComponent).userModule(UserModule()).build().inject(this)
         mPresenter.mView = this
+    }
+
+
+    override fun takeSuccess(result: TResult) {
+        Log.d("TakePhoto", result.image.originalPath)
+        Log.d("TakePhoto", result.image.compressPath)
+
+
+    }
+
+    override fun takeCancel() {
+
+    }
+
+    override fun takeFail(result: TResult?, msg: String?) {
+        Log.e("TakePhoto", msg)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        mTakePhoto.onActivityResult(requestCode, resultCode, data)
+    }
+
+    fun createTempFile() {
+        val tempFileName = "${DateUtils.curTime}.png"
+
+        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+            this.mTempFile = File(Environment.getExternalStorageDirectory(), tempFileName)
+            return
+        }
+
+        this.mTempFile = File(filesDir, tempFileName)
+
+    }
+
+
+    /**
+     * 请求拍照权限
+     */
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    fun requestDangerousPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {//版本判断
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                val strings = arrayOf(
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.RECORD_AUDIO,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+                ActivityCompat.requestPermissions(this, strings, 100)
+            }
+        }
     }
 
 
