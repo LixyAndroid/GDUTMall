3add104b73947b92d4ce633dfb2169a83728fd28
xuyang-happy <958607524@qq.com>
2019-08-20 23:21:29 +0800

    首页

--- App/src/main/java/com/gdut/gdutmall/ui/fragment/HomeFragment.kt
+++ App/src/main/java/com/gdut/gdutmall/ui/fragment/HomeFragment.kt
@@ -1,6 +1,5 @@
 package com.gdut.gdutmall.ui.fragment
 
-import android.app.Activity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
