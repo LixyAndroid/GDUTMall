1c5a2c098ff3d26a1a8608691331bf5951386a8f
xuyang-happy <958607524@qq.com>
2019-08-31 18:22:59 +0800

    使用腾讯云服务器

--- BaseLibrary/src/main/java/com/gdut/base/common/BaseConstant.kt
+++ BaseLibrary/src/main/java/com/gdut/base/common/BaseConstant.kt
@@ -10,9 +10,9 @@ class BaseConstant{
         //七牛服务地址
         const val IMAGE_SERVER_ADDRESS = "http://pw7t5h9o4.bkt.clouddn.com/"
         //本地ip
-        const val SERVER_ADDRESS = "http://10.23.15.6:8080/mall/"
+        //const val SERVER_ADDRESS = "http://10.23.15.6:8080/mall/"
         //腾讯云ip
-        //const val SERVER_ADDRESS = "http://203.195.245.169:8080/GDUTMallServer_war/"
+        const val SERVER_ADDRESS = "http://203.195.245.169:8080/GDUTMallServer_war/"
 
         const val TABLE_PREFS = "GDUT_mall"
 
